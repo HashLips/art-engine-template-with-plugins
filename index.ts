d68aafdddbfaf3d691c265f4695938a87e00bb22
HashLips <hashlipsnft@gmail.com>
2024-02-27 19:23:43 +0200

    feat: :tada: Added the base art engine template

---
+++ index.ts
@@ -0,0 +1,66 @@
+const {
+  ArtEngine,
+  inputs,
+  generators,
+  renderers,
+  exporters,
+} = require("@hashlips-lab/art-engine");
+
+const BASE_PATH = __dirname;
+
+const ae = new ArtEngine({
+  cachePath: `${BASE_PATH}/../cache`,
+  outputPath: `${BASE_PATH}/../output`,
+  useCache: false,
+
+  inputs: {
+    apes: new inputs.ImageLayersInput({
+      assetsBasePath: `${BASE_PATH}/../data`,
+    }),
+  },
+
+  generators: [
+    new generators.ImageLayersAttributesGenerator({
+      dataSet: "apes",
+      startIndex: 1,
+      endIndex: 10,
+    }),
+  ],
+
+  renderers: [
+    new renderers.ItemAttributesRenderer({
+      name: (itemUid: string) => `Ape ${itemUid}`,
+      description: (attributes: any) => {
+        return `This is a token with "${attributes["Background"][0]}" as Background`;
+      },
+    }),
+    new renderers.ImageLayersRenderer({
+      width: 2048,
+      height: 2048,
+    }),
+  ],
+
+  exporters: [
+    new exporters.ImagesExporter(),
+    new exporters.Erc721MetadataExporter({
+      imageUriPrefix: "ipfs://__CID__/",
+    }),
+    new exporters.SolMetadataExporter({
+      imageUriPrefix: "ipfs://__CID__/",
+      symbol: "APES",
+      sellerFeeBasisPoints: 200,
+      collectionName: "The Apes",
+      creators: [
+        {
+          address: "__SOLANA_WALLET_ADDRESS_HERE__",
+          share: 100,
+        },
+      ],
+    }),
+  ],
+});
+
+(async () => {
+  await ae.run();
+  await ae.printPerformance();
+})();
